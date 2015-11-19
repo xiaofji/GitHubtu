a5dbbb81facaf4909bcb73cef924494927485307
xiaofji <xiaofji@umich.edu>
2015-11-19 16:38:48 -0500

    add files
    put the task template here

---
+++ ExtendingTemplates/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template  #NEW IMPORT!!
+
+app = Flask(__name__)    #This is creating a new Flask object
+
+#decorator that links...
+
+@app.route('/')          								#This is the main URL
+def default():
+    return render_template("index.html", name = "index", title = "HOME PAGE")			#The argument should be in templates folder
+
+
+@app.route('/index')          							#This is the main URL
+def index():
+    return render_template("index.html", name = "index", title = "HOME PAGE")			#The argument should be in templates folder
+
+
+@app.route('/courses')          							#This is the main URL
+def courses():
+    return render_template("courses.html", name = "courses", title = "COURSES PAGE")
+
+@app.route('/interests')          							#This is the main URL
+def interests():
+    return render_template("interests.html", name = "interests", title = "INTERESTS PAGE")
+
+@app.route('/other')          							#This is the main URL
+def other():
+    return render_template("other.html", name = "other", title = "OTHER PAGE")
+
+
+
+
+
+@app.route('/<name>')          							#This is the main URL
+def home(name):
+    return render_template("%s.html" % name, name = name)	#The argument should be in templates folder
+
+
+if __name__ == '__main__':
+    app.run(debug=True)		#debug=True is optional
